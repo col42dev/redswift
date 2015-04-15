7dc4cbc1e08fbb3cabc0a64ea1355618883cfe82
Colin Moore <colinmoore.dev@gmail.com>
2015-04-14 17:22:55 -0700

    Initial Commit

---
+++ redswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  redswift
+//
+//  Created by Colin Moore on 14/04/2015.
+//  Copyright (c) 2015 dirtyrect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
